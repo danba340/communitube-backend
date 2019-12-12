4eb16432d7d80c3f7d41e050c66f32b042a26fde
danba340 <danba340@gmail.com>
2019-12-12 01:13:57 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+var http = require("http").createServer();
+var io = require("socket.io")(http);
+
+const roomCounts = {};
+
+io.on("connection", function(socket) {
+  socket.join(socket.handshake.query.sessionId);
+
+  socket.on("paused", function(msg) {
+    io.sockets.in(socket.handshake.query.sessionId).emit("paused", msg);
+  });
+
+  socket.on("playing", function(msg) {
+    io.sockets.in(socket.handshake.query.sessionId).emit("playing", msg);
+  });
+});
+
+http.listen(3001, function() {
+  console.log("listening on *:3001");
+});
