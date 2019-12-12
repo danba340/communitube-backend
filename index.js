02d73c4eae6fae337c40291bfc0a63a9099bec93
danba340 <danba340@gmail.com>
2019-12-12 02:33:34 +0100

    cors another way

--- index.js
+++ index.js
@@ -2,7 +2,14 @@ const express = require("express");
 const app = express();
 var cors = require("cors");
 
-app.use(cors());
+//app.use(cors());
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', "http://"+req.headers.host+':8100');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  next();
+  }
+);
 var http = require("http").createServer(app);
 var io = require("socket.io")(http);
 
